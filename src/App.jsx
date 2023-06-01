759e4bf6e0f8cb17302a98719b7ab8b5b24c1dce
Slimani Imed Eddine Abderrahmane <imeds2000@gmail.com>
2023-06-01 17:23:18 +0100

    todo list app is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,190 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Button,
+  Container,
+  Text,
+  Title,
+  Modal,
+  TextInput,
+  Group,
+  Card,
+  ActionIcon,
+} from '@mantine/core';
+import { useState, useRef, useEffect } from 'react';
+import { IconMoonStars, IconSun, IconTrash } from '@tabler/icons-react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+} from '@mantine/core';
+import { useHotkeys  } from '@mantine/hooks';
+
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+  const [opened, setOpened] = useState(false);
+
+  const [colorScheme, setColorScheme] = useState('light');
+  const toggleColorScheme = (value) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+  useHotkeys([['mod+J', () => toggleColorScheme()]]);
+
+  const taskTitle = useRef('');
+  const taskSummary = useRef('');
+
+  function createTask() {
+    setTasks([
+      ...tasks,
+      {
+        title: taskTitle.current.value,
+        summary: taskSummary.current.value,
+      },
+    ]);
+
+    saveTasks([
+      ...tasks,
+      {
+        title: taskTitle.current.value,
+        summary: taskSummary.current.value,
+      },
+    ]);
+  }
+
+  function deleteTask(index) {
+    var clonedTasks = [...tasks];
+
+    clonedTasks.splice(index, 1);
+
+    setTasks(clonedTasks);
+
+    saveTasks([...clonedTasks]);
+  }
+
+  function loadTasks() {
+    let loadedTasks = localStorage.getItem('tasks');
+
+    let tasks = JSON.parse(loadedTasks);
+
+    if (tasks) {
+      setTasks(tasks);
+    }
+  }
+
+  function saveTasks(tasks) {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}>
+      <MantineProvider
+        theme={{ colorScheme, defaultRadius: 'md' }}
+        withGlobalStyles
+        withNormalizeCSS>
+        <div className='App'>
+          <Modal
+            opened={opened}
+            size={'md'}
+            title={'New Task'}
+            withCloseButton={false}
+            onClose={() => {
+              setOpened(false);
+            }}
+            centered>
+            <TextInput
+              mt={'md'}
+              ref={taskTitle}
+              placeholder={'Task Title'}
+              required
+              label={'Title'}
+            />
+            <TextInput
+              ref={taskSummary}
+              mt={'md'}
+              placeholder={'Task Summary'}
+              label={'Summary'}
+            />
+            <Group mt={'md'} position={'apart'}>
+              <Button
+                onClick={() => {
+                  setOpened(false);
+                }}
+                variant={'subtle'}>
+                Cancel
+              </Button>
+              <Button
+                onClick={() => {
+                  createTask();
+                  setOpened(false);
+                }}>
+                Create Task
+              </Button>
+            </Group>
+          </Modal>
+          <Container size={550} my={40}>
+            <Group position={'apart'}>
+              <Title
+                sx={theme => ({
+                  fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+                  fontWeight: 900,
+                })}>
+                My Tasks
+              </Title>
+              <ActionIcon
+                color={'blue'}
+                onClick={() => toggleColorScheme()}
+                size='lg'>
+                {colorScheme === 'dark' ? (
+                  <IconSun size={16} />
+                ) : (
+                  <IconMoonStars size={16} />
+                )}
+              </ActionIcon>
+            </Group>
+            {tasks.length > 0 ? (
+              tasks.map((task, index) => {
+                if (task.title) {
+                  return (
+                    <Card withBorder key={index} mt={'sm'}>
+                      <Group position={'apart'}>
+                        <Text weight={'bold'}>{task.title}</Text>
+                        <ActionIcon
+                          onClick={() => {
+                            deleteTask(index);
+                          }}
+                          color={'red'}
+                          variant={'transparent'}>
+                          <IconTrash />
+                        </ActionIcon>
+                      </Group>
+                      <Text color={'dimmed'} size={'md'} mt={'sm'}>
+                        {task.summary
+                          ? task.summary
+                          : 'No summary was provided for this task'}
+                      </Text>
+                    </Card>
+                  );
+                }
+              })
+            ) : (
+              <Text size={'lg'} mt={'md'} color={'dimmed'}>
+                You have no tasks
+              </Text>
+            )}
+            <Button
+              onClick={() => {
+                setOpened(true);
+              }}
+              fullWidth
+              mt={'md'}>
+              New Task
+            </Button>
+          </Container>
+        </div>
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
 }
-
-export default App
